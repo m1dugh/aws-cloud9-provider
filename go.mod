db155401dcd6794c74489ede73b6fa6b15e2d3ff
m1dugh <romain103paris@gmail.com>
2023-07-12 11:56:39 +0900

    initial commit: adds main go code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/m1dugh/aws-cloud9-provider
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "context"
+    "flag"
+    "log"
+    "github.com/hashicorp/terraform-plugin-framework/providerserver"
+    "github.com/m1dugh/aws-cloud9-provider/internal/provider"
+)
+
+func main() {
+    fmt.Println("Hello, World")
+}
